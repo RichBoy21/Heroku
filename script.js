493a32533973327153f90511f984b0d28f922373
Rinat <frommrinat@gmail.com>
2023-07-05 22:45:56 +0200

    finished main

---
+++ script.js
@@ -0,0 +1,11 @@
+// const arr = [123, 123, 2, 3, 23, 324];
+// let element = arr[0];
+// for (let i = 0; i < arr.length; i++) {
+  
+//     if (arr[i] > element) {
+//         element = arr[i]
+//     }
+// }console.log(element)
+
+
+console.log('hello')
